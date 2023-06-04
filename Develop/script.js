13347cc4a6691805a81b9272a8d111dd37708dff
jbarz <jbarz0415@gmail.com>
2023-06-04 15:40:43 -0400

    password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,46 @@
 // Assignment code here
-
+var options = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var choice;
+  
+  var userInput = window.prompt ("Enter Password Length (between 8-128 characters): ");
+  if (userInput < 8) {
+    generatePassword()
+  }
+
+  var lowerCase = window.confirm ("Do you want to include lowercase letters? ");
+  if (lowerCase == true) {
+    var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    options.push(...lowerLetters);
+  }
+  
+  var upperCase = window.confirm ("Do you want to inlcude uppercase characters? ");
+  if (upperCase == true) {
+    var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "w", "X", "Y", "Z"];
+    options.push(...upperLetters);
+  }
+
+  var numeric = window.confirm ("Do you want to include numbers? ");
+  if (numeric == true) {
+    var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    options.push(...numeric);
+
+    var otherCharacters = window.confirm ("Do you want to include special characters? ");
+    if (otherCharacters == true) {
+      var otherCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/"];
+      options.push(...otherCharacters);
+    }
+  }
 }
 
+function random () {
+  for (i=0; i < userInput; i++) {
+    
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
